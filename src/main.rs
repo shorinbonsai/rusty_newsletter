e8087ddc6390924f1bf4bade25cec29eac5862ce
James <shorinbonsai@gmail.com>
2023-11-14 15:42:01 -0500

    first working integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use rusty_newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
